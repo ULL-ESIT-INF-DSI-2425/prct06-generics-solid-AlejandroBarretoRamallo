a16bb9a0cd0dbaa83fc50d12e9fe1f73e50e30f7
AlejandroBarretoRamallo <alu0101552774@ull.edu.es>
2025-03-12 18:52:31 +0000

    Adicion del coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vitest/config'
+
 export default defineConfig({
   test: {
-    include: ['tests/**/*.spec.ts'],  // Aquí especificamos tus archivos de prueba .spec.ts
+    include: ['tests/**/*.spec.ts'],  // Aquí especificamos tus archivos de       prueba .spec.ts
     coverage: {
       provider: 'v8',  // Usar el recolector de cobertura V8
-      reporter: ['text', 'html'],  // Reportes en formato texto y HTML
-      include: ['src/**/*.ts', 'src/*.ts'],  // Solo incluir archivos TS en src para la cobertura
-      exclude: ['**/*.spec.ts'],  // Excluir los archivos .spec.ts de la cobertura
+      reporter: ['text', 'html', 'lcov', 'cobertura'],  // Agregar 'lcov' y       'cobertura'
+      include: ['src/**/*.ts', 'src/*.ts'],  
+      exclude: ['**/*.spec.ts'],  
     },
   },
 })
