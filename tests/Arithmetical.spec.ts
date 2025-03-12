4445fad46de4d19c110898f173bb83ec4aa5e4de
AlejandroBarretoRamallo <alu0101552774@ull.edu.es>
2025-03-12 12:14:09 +0000

    Pr6

--- tests/Arithmetical.spec.ts
+++ tests/Arithmetical.spec.ts
@@ -1,5 +1,5 @@
 import {expect, test, describe} from 'vitest'
-import {Arithmeticable, ArithmeticableCollection, complex, ComplexNumber} from '../src/Arithmeticable'
+import { ArithmeticableCollection, ComplexNumber} from '../src/Arithmeticable'
 
 describe('Tests de la clase ArithmeticalColection y ComplexNumber', () => {
   let complex = new ComplexNumber([1, 1])
@@ -22,10 +22,20 @@ describe('Tests de la clase ArithmeticalColection y ComplexNumber', () => {
   test('Metodos de la clase ComplexNumber', () => {
     let operand: ComplexNumber = new ComplexNumber([1, 1])
     expect(complex.add(operand)).toEqual(new ComplexNumber([2, 2]))
+    expect(complex.add(new ComplexNumber([0, 0]))).toEqual(new ComplexNumber([1, 1]))
+    expect(complex.add(new ComplexNumber([-1, -1]))).toEqual(new ComplexNumber([0, 0]))
+    expect(complex.add(new ComplexNumber([-2, -2]))).toEqual(new ComplexNumber([-1, -1]))
     expect(complex.sub(operand)).toEqual(new ComplexNumber([0, 0]))
+    expect(complex.sub(new ComplexNumber([0, 0]))).toEqual(new ComplexNumber([1, 1]))
+    expect(complex.sub(new ComplexNumber([-1, -1]))).toEqual(new ComplexNumber([2, 2]))
+    expect(complex.sub(new ComplexNumber([-2, -2]))).toEqual(new ComplexNumber([3, 3]))
     expect(complex.multiply(operand)).toEqual(new ComplexNumber([2, 2]))
     expect(complex.multiply(operand)).toEqual(new ComplexNumber([2, 2]))
+    expect(complex.multiply(new ComplexNumber([1, 1]))).toEqual(new ComplexNumber([2, 2]))
+    expect(complex.multiply(new ComplexNumber([2, 2]))).toEqual(new ComplexNumber([4, 4]))
     expect(complex.divide(new ComplexNumber([0, 0]))).toEqual(undefined)
     expect(complex.divide(new ComplexNumber([0, 1]))).toEqual(new ComplexNumber([1, 1]))
+    expect(complex.divide(new ComplexNumber([1, 0]))).toEqual(new ComplexNumber([1, -1]))
+    expect(complex.divide(new ComplexNumber([2, 0]))).toEqual(new ComplexNumber([0.5, -1]))
   })
 })
